9a8aa2931a2c26bee6194911f358078b7173f579
rheintzmann <heintzmann@gmail.com>
2019-08-28 12:22:36 +0200

    Bug-fix in reorient.m

--- reorient.m
+++ reorient.m
@@ -2,7 +2,6 @@ function res=reorient(vec,d)
 % res=reorient(vec,d) : reorients a 1D dipimage in space
 % vec : vector to orient
 % d : dimension
-    sizes=ones(1,d);
-    sizes(d) = prod(size(vec));
-    res= reshape(vec,sizes);
-    
+sizes=ones(1,max(ndims(vec),max(d,2)));
+sizes(d) = prod(size(vec));
+res= reshape(vec,sizes);
